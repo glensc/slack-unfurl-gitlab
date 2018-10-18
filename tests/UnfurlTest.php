7860da66df4a996527b4217d2a5ea211cfe880e5
Elan Ruusamäe <glen@delfi.ee>
2018-10-18 21:34:48 +0300

    merge two unfurl tests to single class

--- tests/UnfurlTest.php
+++ tests/UnfurlTest.php
@@ -2,24 +2,65 @@
 
 namespace GitlabSlackUnfurl\Test;
 
-use GitlabSlackUnfurl\Route\MergeRequest;
+use GitlabSlackUnfurl\Route;
 
-class MergeUnfurlTest extends TestCase
+class UnfurlTest extends TestCase
 {
     /**
      * @param string $url
      * @param array $parts
      * @param array $expected
-     * @dataProvider dataProvider
+     * @dataProvider issueDataProvider
      */
-    public function testMergeRequest(string $url, array $parts, array $expected): void
+    public function testIssueUnfurl(string $url, array $parts, array $expected): void
     {
-        $unfurler = $this->app[MergeRequest::class];
+        $unfurler = $this->app[Route\Issue::class];
         $result = $unfurler->unfurl($url, $parts);
         $this->assertEquals($expected, $result);
     }
 
-    public function dataProvider()
+    /**
+     * @param string $url
+     * @param array $parts
+     * @param array $expected
+     * @dataProvider mergeDataProvider
+     */
+    public function testMergeRequest(string $url, array $parts, array $expected): void
+    {
+        $unfurler = $this->app[Route\MergeRequest::class];
+        $result = $unfurler->unfurl($url, $parts);
+        $this->assertEquals($expected, $result);
+    }
+
+    public function issueDataProvider(): array
+    {
+        return [
+            [
+                'https://gitlab.com/gitlab-org/gitlab-ce/issues/12733',
+                [
+                    'namespace' => 'gitlab-org/',
+                    'project_path' => 'gitlab-org/gitlab-ce',
+                    'repo' => 'gitlab-ce',
+                    'number' => '12733',
+                ],
+                [
+                    'title' => '<https://gitlab.com/gitlab-org/gitlab-ce/issues/12733|#12733>: Disable Unfurling for the login page',
+                    'color' => '#E24329',
+                    'ts' => 1453816324,
+                    'footer' => 'Created by <https://gitlab.com/jvanbaarsen|Jeroen van Baarsen>',
+                    'fields' => [
+                        [
+                            'title' => 'Labels',
+                            'value' => 'Platform, external services, feature proposal',
+                            'short' => true,
+                        ],
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    public function mergeDataProvider(): array
     {
         return [
             [
